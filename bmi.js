26c82154d736f104c66d09f104aab5f8ec6ea9b1
Nguyen Hiep <hiephap1705@gmail.com>
2023-10-04 21:24:39 +0700

    week 3

--- bmi.js
+++ bmi.js
@@ -1,26 +1,16 @@
 const calculateBMI = (weight, height) => {
-
-
     if (weight < 40 || height < 1.5 || weight > 90 || height > 1.9) {
         return "Đầu vào không hợp lệ"
     }
-
-    // Tính chỉ số BMI
-    var bmi = weight / (height * height);
-
-
-
+    let bmi = weight / (height * height);
     bmi = bmi.toFixed(2);
-
     if (bmi < 16.5) {
         return "Dưới Chuẩn"
     }
-    // else return "Chuẩn" (Sửa lỗi)
     else if (bmi <= 25) {
         return "Chuẩn"
     }
     else return "Béo"
-
 }
 console.log(calculateBMI(20, 1.8))
 module.exports = calculateBMI
